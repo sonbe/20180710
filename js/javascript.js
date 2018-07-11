cc5a07f55b6dffafc08b711f64fbe36d209dfc06
sonbe <32283310+sonbe@users.noreply.github.com>
2018-07-11 09:27:25 +0900

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
